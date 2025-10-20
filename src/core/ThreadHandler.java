25bf745060c828b6e60d113b8f95eefb9b088e79
Jun Yeong Kim <junyeonggim5@gmail.com>
2025-10-20 23:15:24 +0900

    Edit some compile errors
    In the last patch, core.Thread class has been
    changed into LogFilterTask but the source code
    didn't changed.
    
    Signed-off-by: Jun Yeong Kim <junyeonggim5@gmail.com>

--- src/core/ThreadHandler.java
+++ src/core/ThreadHandler.java
@@ -4,13 +4,12 @@ import config.AlfaConfig;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class ThreadHandler {
 
     // The threads list holds core.Thread (Runnable) objects.
-    private List<Thread> threads = new ArrayList<>();
+    private List<LogFilterTask> threads = new ArrayList<>();
     private AlfaConfig config;
 
     public ThreadHandler(AlfaConfig config) {
@@ -20,12 +19,12 @@ public class ThreadHandler {
         // For every configured file path (symbol)
         for (String symbol : absPathSymbols) {
             // Create a core.Thread (Runnable) object (modified in step 1) and add it to the list.
-            threads.add(new core.Thread(config, symbol));
+            threads.add(new LogFilterTask(config, symbol));
         }
     }
 
     // Add a getter so that external classes (e.g., BatchHandler) can retrieve the list of tasks.
-    public List<Thread> getRunnableTasks() {
+    public List<LogFilterTask> getRunnableTasks() {
         return threads;
     }
 }
